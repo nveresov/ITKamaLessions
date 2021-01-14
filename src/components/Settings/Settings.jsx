807523e167ae811976655c3b3a1e2a841e551605
nveresov <nveresov@gmail.com>
2021-01-14 12:26:56 +0300

    Lesson 19

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+import s from './Settings.module.css'
+
+const Settings = () => {
+    return <div className={s.content}>
+        Settings
+    </div>
+}
+
+export default Settings;
